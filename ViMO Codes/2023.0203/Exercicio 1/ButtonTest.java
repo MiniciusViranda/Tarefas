0d4be73948e0adca80789c93a18c9788e276dda7
Minicius Viranda <124306959+MiniciusViranda@users.noreply.github.com>
2023-03-02 19:04:13 -0300

    Add files via upload

---
+++ ViMO Codes/2023.0203/Exercicio 1/ButtonTest.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class ButtonTest {
+
+    public static void main(String[] args) {
+        ButtonFrame buttonFrame = new ButtonFrame();
+        buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonFrame.setSize(280,220);
+        buttonFrame.setLocationRelativeTo(null);
+        buttonFrame.setVisible(true);
+    }
+}
+
+/*ViMO*/
